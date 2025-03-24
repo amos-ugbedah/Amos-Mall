8ee69b4b631557e2b9741136c0af743cbd37aca3
Ugbedah Amos <ugbedahamos@gmail.com>
2025-03-25 00:04:59 +0100

    added change

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { CartProvider } from "./context/CartContext";
+import { CartProvider } from "./Context/CartContext.jsx"; // ✅ Fixed import path
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
