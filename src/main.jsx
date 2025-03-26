2a6734cc4b31547a7ff958cdd4657ec3c8bc6963
Ugbedah Amos <ugbedahamos@gmail.com>
2025-03-26 20:04:49 +0100

    new update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
+import { WishlistProvider } from "./context/WishlistContext"; // Add this import if you're using it
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { CartProvider } from "./context/CartContext"; // ✅ No `.jsx`
-
+import { CartProvider } from "./context/CartContext";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CartProvider>
-      <App />
+      <WishlistProvider>
+        <App />
+      </WishlistProvider>
     </CartProvider>
   </React.StrictMode>
 );
