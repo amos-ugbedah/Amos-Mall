6b87d20f3098d13df4c2026aab8960ef8a2ca3c1
Ugbedah Amos <ugbedahamos@gmail.com>
2025-03-26 23:00:11 +0100

    context

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { CartProvider } from "./context/CartContext";
 import { WishlistProvider } from "./context/WishlistContext";
 import { CurrencyProvider } from "./context/CurrencyContext";
-import "./index.css"; // ✅ Make sure Tailwind is imported
+import "./index.css"; // Ensure Tailwind is imported
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
